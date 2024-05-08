2eef7a3ff6eadac2cabce4b601565eeba27cfc42
Mohammed Shareef <shareef.mk03@gmail.com>
2024-05-08 10:01:12 +0530

    initial setup

---
+++ vndr_mgmt_sys/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for vndr_mgmt_sys project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vndr_mgmt_sys.settings')
+
+application = get_asgi_application()
